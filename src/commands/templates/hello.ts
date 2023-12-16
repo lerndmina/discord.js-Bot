952a6ccdadb9851296640355579680c668673400
Adam <wild@lerndmina.dev>
2023-12-16 01:07:33 +0000

    Updated template command

--- src/commands/templates/hello.ts
+++ src/commands/templates/hello.ts
@@ -1,10 +1,12 @@
-import type { CommandData, SlashCommandProps, CommandOptions } from 'commandkit';
-import { SlashCommandBuilder, EmbedBuilder } from "discord.js"
-import log from 'fancy-log';
-import { waitingEmoji } from '../../Bot';
+import type { CommandData, SlashCommandProps, CommandOptions } from "commandkit";
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import log from "fancy-log";
+import { waitingEmoji } from "../../Bot";
 
-
-export const data = new SlashCommandBuilder().setName("hello").setDescription("This is a template command.");
+export const data = new SlashCommandBuilder()
+  .setName("hello")
+  .setDescription("This is a template command.")
+  .setDMPermission(false);
 
 export const options: CommandOptions = {
   devOnly: true,
@@ -12,7 +14,7 @@ export const options: CommandOptions = {
   deleted: false,
 };
 
-export async function run({interaction, client, handler}: SlashCommandProps) {
-  await interaction.reply({ content: waitingEmoji, ephemeral: true});
+export async function run({ interaction, client, handler }: SlashCommandProps) {
+  await interaction.reply({ content: waitingEmoji, ephemeral: true });
   interaction.editReply({ content: "Loading spinner complete" });
-};
+}
