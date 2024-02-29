56945befc7b1534e1a3dbc3fb4ac3335ad18d284
Adam <wild@lerndmina.dev>
2024-02-29 16:42:07 +0000

    Made it more concise

--- src/events/messageCreate/handleTimezones.ts
+++ src/events/messageCreate/handleTimezones.ts
@@ -20,24 +20,24 @@ export default async function (message: Message, client: Client<true>) {
 
   log("We found a time! Parsed time: " + data.date);
 
-  const embed = BasicEmbed(
-    client,
-    "Found Time!",
-    // prettier-ignore
-    `This looks like a time! I've parsed it as: <t:${data.seconds}:F> \nIf you want to send timestamps yourself, you can!\n Just use \`/getTime\` and I'll return you a discord timestamp from your message. \n\nI'll remove the embed in 60s and leave the timestamp.\n\nHere's some more details about this, in case something is wrong. . .\n\`\`\`json\n${JSON.stringify(data, null ,2)}\`\`\``
-  );
+  // const embed = BasicEmbed(
+  //   client,
+  //   "Found Time!",
+  //   // prettier-ignore
+  //   `This looks like a time! I've parsed it as: <t:${data.seconds}:F> \nIf you want to send timestamps yourself, you can!\n Just use \`/getTime\` and I'll return you a discord timestamp from your message. \n\nI'll remove the embed in 60s and leave the timestamp.\n\nHere's some more details about this, in case something is wrong. . .\n\`\`\`json\n${JSON.stringify(data, null ,2)}\`\`\``
+  // );
 
-  const content = `⏰ <t:${data.seconds}:F>`;
+  const content = `Converted to timestamp: ⏰ <t:${data.seconds}:F>`;
 
-  const reply = await message.reply({ content, embeds: [embed] });
+  const reply = await message.reply({ content });
 
-  sleep(60 * 1000).then(() => {
-    try {
-      reply.edit({ content, embeds: [] });
-    } catch (error) {
-      // We don't care if this fails, it's just a cleanup.
-    }
-  });
+  // sleep(60 * 1000).then(() => {
+  //   try {
+  //     reply.edit({ content, embeds: [] });
+  //   } catch (error) {
+  //     // We don't care if this fails, it's just a cleanup.
+  //   }
+  // });
 
   return false;
 }
