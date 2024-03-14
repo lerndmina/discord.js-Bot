1a30f4113013144fa9ad994b686000d4984093ba
Adam <wild@lerndmina.dev>
2024-03-14 15:48:38 +0000

    Implemented caching

--- src/utils/ParseTimeFromMessage.ts
+++ src/utils/ParseTimeFromMessage.ts
@@ -1,11 +1,14 @@
 import { Message } from "discord.js";
 import * as chrono from "chrono-node";
 import moment from "moment-timezone";
-import * as UserTimezone from "../models/UserTimezone";
+import UserTimezone from "../models/UserTimezone";
 import FetchEnvs from "./FetchEnvs";
+import Database from "./data/database";
 const env = FetchEnvs();
 
-export default async function (message: Message | string | { content: string, author: { id: string }}): Promise<
+export default async function (
+  message: Message | string | { content: string; author: { id: string } }
+): Promise<
   | {
       success: true;
       message: string;
@@ -17,14 +20,14 @@ export default async function (message: Message | string | { content: string, au
       message: string;
       date: null;
       seconds: null;
-    }> {
-
+    }
+> {
   const content = typeof message === "string" ? message : message.content;
   const author = typeof message === "string" ? { id: undefined } : message.author;
-      
+
   // Try to parse the date from the message
   let parsed = chrono.uk.parse(content, {
-    timezone: 0 // Parse all dates in UTC, we'll apply the timezone offset later
+    timezone: 0, // Parse all dates in UTC, we'll apply the timezone offset later
   })[0];
 
   console.log(parsed);
@@ -42,11 +45,12 @@ export default async function (message: Message | string | { content: string, au
   let tz = env.DEFAULT_TIMEZONE;
 
   // If we have an author, check if they have a timezone set
-  const authorId = author.id; 
-  if(authorId) {
+  const authorId = author.id;
+  if (authorId) {
+    const db = new Database();
+
     try {
-      const query = UserTimezone.default.findOne({ userId: authorId });
-      const response = await query.exec();
+      const response = await db.findOne(UserTimezone, { userId: authorId });
 
       if (!response) {
         console.log("No timezone found, assuming ", tz);
@@ -65,7 +69,13 @@ export default async function (message: Message | string | { content: string, au
   let date = moment.utc(parsed.start.date());
   // @ts-ignore
   if (parsed.start.knownValues.timezoneOffset === undefined) {
-    console.log("No timezone offset found in the message, subtracting", tzOffset, "minutes to account for the timezone. . . (", tz, ")");
+    console.log(
+      "No timezone offset found in the message, subtracting",
+      tzOffset,
+      "minutes to account for the timezone. . . (",
+      tz,
+      ")"
+    );
     date = date.add(-tzOffset, "minutes");
   }
 
