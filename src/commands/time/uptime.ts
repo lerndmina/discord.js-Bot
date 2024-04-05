d8b85cc316a891b5889e02540eafed2a5611c9d9
Wild <wild@lerndmina.dev>
2024-04-05 17:47:01 +0100

    Fix uptime

--- src/commands/time/uptime.ts
+++ src/commands/time/uptime.ts
@@ -1,11 +1,11 @@
 import type { CommandData, SlashCommandProps, CommandOptions } from "commandkit";
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import log from "fancy-log";
-import { globalCooldownKey, setCommandCooldown, waitingEmoji } from "../../Bot";
+import { globalCooldownKey, redisClient, setCommandCooldown, waitingEmoji } from "../../Bot";
 import ParseTimeFromMessage from "../../utils/ParseTimeFromMessage";
 import BasicEmbed from "../../utils/BasicEmbed";
-import { lastRestart } from "../../events/ready/loggedIn";
-import { msToTime } from "../../utils/TinyUtils";
+import { debugMsg } from "../../utils/TinyUtils";
+import prettyMilliseconds from "pretty-ms";
 
 export const data = new SlashCommandBuilder()
   .setName("uptime")
@@ -18,9 +18,17 @@ export const options: CommandOptions = {
 };
 
 export async function run({ interaction, client, handler }: SlashCommandProps) {
+  const lastRestart = parseInt((await redisClient.get("lastRestart")) ?? "0");
+  const now = Date.now();
+  const uptime = prettyMilliseconds(now - lastRestart, {
+    verbose: true,
+    unitCount: 2,
+    secondsDecimalDigits: 0,
+    millisecondsDecimalDigits: 0,
+    separateMilliseconds: true,
+  });
+
   return interaction.reply({
-    embeds: [
-      BasicEmbed(client, "Uptime", `I have been online for ${msToTime(Date.now() - lastRestart)}`),
-    ],
+    embeds: [BasicEmbed(client, "Uptime", `I was last restarted ${uptime} ago.`)],
   });
 }
