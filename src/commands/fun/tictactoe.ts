5a06a6ef9a06f0b630ba8b3f859c250dda5c3ba6
Wild <wild@lerndmina.dev>
2024-04-18 16:34:21 +0100

    Added TicTacToe

---
+++ src/commands/fun/tictactoe.ts
@@ -0,0 +1,267 @@
+import { SlashCommandProps, CommandOptions, ButtonKit } from "commandkit";
+import {
+  ActionRow,
+  ActionRowBuilder,
+  BaseInteraction,
+  ButtonBuilder,
+  ButtonStyle,
+  Client,
+  Collection,
+  SlashCommandBuilder,
+} from "discord.js";
+import log from "fancy-log";
+import { globalCooldownKey, setCommandCooldown, userCooldownKey, waitingEmoji } from "../../Bot";
+import generateHelpFields from "../../utils/data/static/generateHelpFields";
+import Database from "../../utils/data/database";
+import TicTacToeSchema, { TicTacToeSchemaType } from "../../models/TicTacToeSchema";
+import { debugMsg } from "../../utils/TinyUtils";
+import BasicEmbed from "../../utils/BasicEmbed";
+
+const db = new Database();
+
+export const TTT_X = "X";
+export const TTT_O = "O";
+export const TTT_BLANK = "⠀";
+
+export const data = new SlashCommandBuilder()
+  .setName("tictactoe")
+  .setDescription("Play a game of Tic Tac Toe.")
+  .addUserOption((option) =>
+    option.setName("opponent").setDescription("The user to play against.").setRequired(true)
+  )
+  .addIntegerOption((option) =>
+    option.setName("size").setDescription("The size of the board.").setRequired(false)
+  )
+  .setDMPermission(false);
+
+export const options: CommandOptions = {
+  devOnly: false,
+  deleted: false,
+};
+
+const MIN_SIZE = 3;
+const MAX_SIZE = 5;
+
+export async function run({ interaction, client, handler }: SlashCommandProps) {
+  const commandName = interaction.commandName;
+
+  const size = interaction.options.getInteger("size", false) || 3;
+  if (size < MIN_SIZE || size > MAX_SIZE) {
+    interaction.reply({
+      content: `The size of the board must be between ${MIN_SIZE} and ${MAX_SIZE}.`,
+      ephemeral: true,
+    });
+    return;
+  }
+
+  const opponent = interaction.options.getUser("opponent", true);
+  const challenger = interaction.user;
+  if (opponent.bot) {
+    interaction.reply({
+      content: "You cannot play against a bot.",
+      ephemeral: true,
+    });
+    return;
+  }
+  if (opponent.id === interaction.user.id) {
+    interaction.reply({
+      content: "You cannot play against yourself.",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  setCommandCooldown(userCooldownKey(interaction.user.id, commandName), 30);
+
+  await interaction.reply({
+    content: `Inviting <@${opponent.id}> to play a game of Tic Tac Toe...`,
+    ephemeral: true,
+  });
+
+  const buttonAccept = new ButtonKit()
+    .setEmoji("👍")
+    .setLabel("Accept")
+    .setStyle(ButtonStyle.Primary)
+    .setCustomId("pre-tictactoe-accept" + interaction.id);
+
+  const buttonDecline = new ButtonKit()
+    .setEmoji("👎")
+    .setLabel("Decline")
+    .setStyle(ButtonStyle.Danger)
+    .setCustomId("pre-tictactoe-decline" + interaction.id);
+
+  const acceptDeclineRow = new ActionRowBuilder<ButtonKit>().addComponents(
+    buttonAccept,
+    buttonDecline
+  );
+
+  const message = await interaction.channel!.send({
+    content: `<@${opponent.id}>`,
+    components: [acceptDeclineRow],
+    embeds: [
+      BasicEmbed(
+        client,
+        "Tic Tac Toe",
+        `Hey <@${opponent.id}> you have been challenged by <@${interaction.user.id}> to play a game of Tic Tac Toe with a board size of \`${size}\`.`,
+        [
+          {
+            name: "Accept",
+            value: "Click the button below to accept the challenge.",
+            inline: true,
+          },
+          {
+            name: "Decline",
+            value: "Click the button below to decline the challenge.",
+            inline: true,
+          },
+        ]
+      ),
+    ],
+  });
+
+  buttonAccept.onClick(
+    async (interaction) => {
+      if (interaction.user.id !== opponent.id) {
+        interaction.reply({ content: "Only the opponent can accept or decline.", ephemeral: true });
+        return;
+      }
+      const { rows, gameState } = initiateGameState(size, interaction);
+
+      const turn = Math.random() > 0.5 ? challenger.id : opponent.id;
+
+      try {
+        interaction.deferUpdate();
+
+        const data: TicTacToeSchemaType = {
+          guildId: message.guildId!,
+          messageId: message.id,
+          channelId: message.channelId,
+          initiatorId: challenger.id,
+          opponentId: opponent.id,
+          size,
+          gameState,
+          turn,
+          gameOver: false,
+          createdAt: new Date(),
+        };
+
+        await message.edit({
+          content: ``,
+          components: rows,
+          embeds: [getTicTacToeEmbed(data, client)],
+        });
+
+        await db.findOneAndUpdate(TicTacToeSchema, { messageId: data.messageId }, data);
+        setCommandCooldown(userCooldownKey(interaction.user.id, commandName), 120);
+        debugMsg(`Game set up for ${interaction.user.tag} and ${opponent.tag}`);
+      } catch (error) {
+        log.error(error);
+        await interaction.reply({
+          content: "An error occurred while setting up the game.",
+          ephemeral: true,
+        });
+
+        await db.deleteOne(TicTacToeSchema, { initiatorId: interaction.user.id });
+        return;
+      }
+    },
+    { message }
+  );
+  buttonDecline.onClick(
+    async (interaction) => {
+      if (interaction.user.id !== opponent.id) {
+        interaction.reply({ content: "Only the opponent can accept or decline.", ephemeral: true });
+        return;
+      }
+      interaction.deferUpdate();
+      message.edit({
+        components: [],
+        content: ``,
+        embeds: [
+          BasicEmbed(
+            client,
+            "Tic Tac Toe",
+            `The game has been cancelled, <@${opponent.id}> declined the challenge.`
+          ),
+        ],
+      });
+    },
+    { message }
+  );
+}
+function initiateGameState(size: number, interaction: BaseInteraction) {
+  const gameState: Record<`${number}${number}`, string> = {};
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      gameState[`${x}${y}`] = TTT_BLANK;
+    }
+  }
+
+  const rows = getButtonsForGameState(gameState, size, interaction);
+
+  return { gameState, rows };
+}
+
+export function getButtonsForGameState(
+  gameState: Record<`${number}${number}`, string>,
+  size: number,
+  interaction: BaseInteraction,
+  disableAll: boolean = false
+) {
+  const rows: ActionRowBuilder<ButtonBuilder>[] = [];
+
+  for (let x = 0; x < size; x++) {
+    let buttons: ButtonBuilder[] = [];
+    for (let y = 0; y < size; y++) {
+      let buttonStyle: ButtonStyle;
+      if (gameState[`${x}${y}`] === TTT_O) {
+        buttonStyle = ButtonStyle.Success;
+      } else if (gameState[`${x}${y}`] === TTT_X) {
+        buttonStyle = ButtonStyle.Danger;
+      } else {
+        buttonStyle = ButtonStyle.Primary;
+      }
+      let button = new ButtonBuilder()
+        .setCustomId(`tictactoe_${x}_${y}_${interaction.id}`)
+        .setLabel(gameState[`${x}${y}`] || TTT_BLANK)
+        .setStyle(buttonStyle)
+        .setDisabled(disableAll);
+      buttons.push(button);
+    }
+    rows.push(new ActionRowBuilder<ButtonBuilder>().addComponents(buttons));
+  }
+
+  return rows;
+}
+
+export function getTicTacToeEmbed(
+  game: TicTacToeSchemaType,
+  client: Client<true>,
+  isDraw?: boolean,
+  { winnerId, looserId }: { winnerId?: string; looserId?: string } = {}
+) {
+  if (isDraw) {
+    return BasicEmbed(
+      client,
+      "Tic Tac Toe",
+      `The game between <@${game.initiatorId}> and <@${game.opponentId}> resulted in a draw! GG!`
+    );
+  }
+  if (winnerId) {
+    const possibleWinnerMessages = [
+      `The game between <@${game.initiatorId}> and <@${game.opponentId}> resulted in a win for <@${winnerId}>! GG!`,
+      `<@${winnerId}> won the game against <@${looserId}>! GG!`,
+      `<@${winnerId}> destroyed <@${looserId}>!`,
+      `What a fine display of skill <@${winnerId}>! GG!`,
+    ];
+    const winnerMessage =
+      possibleWinnerMessages[Math.floor(Math.random() * possibleWinnerMessages.length)];
+    return BasicEmbed(client, "Tic Tac Toe", winnerMessage);
+  }
+
+  return BasicEmbed(
+    client,
+    "Tic Tac Toe",
+    `<@${game.initiatorId}> vs <@${game.opponentId}>\n\nTurn: <@${game.turn}>`
+  );
+}
---
+++ src/events/interactionCreate/ticTacToeHandler.ts
@@ -0,0 +1,177 @@
+import {
+  Base,
+  BaseInteraction,
+  ChannelType,
+  Client,
+  InteractionType,
+  Message,
+  MessageComponentInteraction,
+  RepliableInteraction,
+} from "discord.js";
+import Database from "../../utils/data/database";
+import TicTacToeSchema, { TicTacToeSchemaType } from "../../models/TicTacToeSchema";
+import {
+  TTT_BLANK,
+  TTT_O,
+  TTT_X,
+  getButtonsForGameState,
+  getTicTacToeEmbed,
+} from "../../commands/fun/tictactoe";
+import { debugMsg } from "../../utils/TinyUtils";
+import FetchEnvs from "../../utils/FetchEnvs";
+const db = new Database();
+const env = FetchEnvs();
+
+export default async (interaction: MessageComponentInteraction, client: Client<true>) => {
+  if (interaction.type != InteractionType.MessageComponent) return;
+  if (!interaction.channel || !interaction.channel.isTextBased()) return;
+  if (!interaction.guild) return;
+  if (!interaction.customId.startsWith("tictactoe_")) return;
+
+  const message = await interaction.channel.messages.fetch(interaction.message.id);
+  if (!message)
+    return interaction.reply({ content: "The message was not found.", ephemeral: true });
+
+  let game = (await db.findOne(TicTacToeSchema, {
+    messageId: interaction.message.id,
+  })) as TicTacToeSchemaType;
+  if (!game) return interaction.reply({ content: "The game was not found.", ephemeral: true });
+
+  if (interaction.user.id !== game.initiatorId && interaction.user.id !== game.opponentId)
+    return interaction.reply({ content: "You are not part of this game.", ephemeral: true });
+
+  if (game.gameOver)
+    return interaction.reply({ content: "The game is already over.", ephemeral: true });
+
+  if (game.turn !== interaction.user.id)
+    return interaction.reply({ content: "It's not your turn.", ephemeral: true });
+
+  // prettier-ignore
+  const clickedLocation = `${interaction.customId.split("_")[1]}${interaction.customId.split("_")[2]}`;
+
+  if (game.gameState[clickedLocation] !== TTT_BLANK)
+    return interaction.reply({ content: "This location is already taken.", ephemeral: true });
+
+  game.gameState[clickedLocation] = game.turn === game.initiatorId ? TTT_X : TTT_O;
+
+  // Set the turn to the opponent
+  game.turn = game.turn === game.initiatorId ? game.opponentId : game.initiatorId;
+
+  const rows = getButtonsForGameState(game.gameState, game.size, interaction);
+  const embed = getTicTacToeEmbed(game, client);
+
+  const winner = checkWin(game, game.size);
+  if (winner) {
+    endGame(game, message, interaction, interaction.client, winner);
+    return;
+  } else if (checkDraw(game, game.size)) {
+    endGame(game, message, interaction, interaction.client, null);
+    return;
+  }
+
+  debugGameState(game, game.size);
+  await db.findOneAndUpdate(TicTacToeSchema, { messageId: game.messageId }, game);
+
+  message.edit({
+    components: rows,
+    embeds: [embed],
+  });
+
+  interaction.deferUpdate();
+};
+
+function endGame(
+  game: TicTacToeSchemaType,
+  message: Message,
+  interaction: MessageComponentInteraction,
+  client: Client<true>,
+  winner: string | null
+) {
+  interaction.deferUpdate();
+  const rows = getButtonsForGameState(game.gameState, game.size, interaction, true);
+
+  if (winner) {
+    const winnerId = winner === TTT_X ? game.initiatorId : game.opponentId;
+    const looserId = winnerId === game.initiatorId ? game.opponentId : game.initiatorId;
+    const embed = getTicTacToeEmbed(game, client, false, { winnerId, looserId });
+    message.edit({
+      content: "",
+      embeds: [embed],
+      components: rows,
+    });
+  } else {
+    message.edit({
+      content: ``,
+      embeds: [getTicTacToeEmbed(game, client, true)],
+      components: rows,
+    });
+  }
+
+  game.gameOver = true;
+  db.findOneAndUpdate(TicTacToeSchema, { messageId: game.messageId }, game);
+  db.cleanCache(`${env.MONGODB_DATABASE}:${TicTacToeSchema.name}:messageId:${game.messageId}`);
+}
+
+function debugGameState(game: TicTacToeSchemaType, size: number) {
+  if (!env.DEBUG_LOG) return;
+  const board = Array.from({ length: size }, () => Array(size).fill("⬜"));
+  for (let [key, value] of Object.entries(game.gameState)) {
+    const x = parseInt(key[0], 10);
+    const y = parseInt(key[1], 10);
+    if (value === TTT_BLANK) value = "_";
+    board[x][y] = value;
+  }
+
+  // Print each row of the board
+  for (const row of board) {
+    console.log(row.join(" "));
+  }
+}
+
+function checkWin(game: TicTacToeSchemaType, size: number): string | null {
+  const gameState = game.gameState;
+  const board = Array.from({ length: size }, () => Array(size).fill("⬜"));
+  for (const [key, value] of Object.entries(gameState)) {
+    const x = parseInt(key[0], 10);
+    const y = parseInt(key[1], 10);
+    board[x][y] = value;
+  }
+
+  // Check rows and columns
+  for (let i = 0; i < size; i++) {
+    if (board[i].every((value) => value === TTT_X)) {
+      return TTT_X;
+    }
+    if (board[i].every((value) => value === TTT_O)) {
+      return TTT_O;
+    }
+    if (board.every((row) => row[i] === TTT_X)) {
+      return TTT_X;
+    }
+    if (board.every((row) => row[i] === TTT_O)) {
+      return TTT_O;
+    }
+  }
+
+  // Check diagonals
+  if (board.every((row, i) => row[i] === TTT_X)) {
+    return TTT_X;
+  }
+  if (board.every((row, i) => row[i] === TTT_O)) {
+    return TTT_O;
+  }
+  if (board.every((row, i) => row[size - i - 1] === TTT_X)) {
+    return TTT_X;
+  }
+  if (board.every((row, i) => row[size - i - 1] === TTT_O)) {
+    return TTT_O;
+  }
+
+  return null;
+}
+
+function checkDraw(game: TicTacToeSchemaType, size: number): boolean {
+  const gameState = game.gameState;
+  const allCellsFilled = Object.values(gameState).every((value) => value !== TTT_BLANK);
+  return allCellsFilled && !checkWin(game, size);
+}
